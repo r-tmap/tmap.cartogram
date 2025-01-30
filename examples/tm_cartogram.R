14439aae598cd6979315b35cf72e7ba57b6ecd7d
mtennekes <mtennekes@gmail.com>
2025-01-30 22:08:32 +0100

    added vig, testing gh-pages

--- examples/tm_cartogram.R
+++ examples/tm_cartogram.R
@@ -1,12 +1,11 @@
+\dontrun{
 library(tmap)
 
 Africa = World[World$continent == "Africa", ]
 
-#step3 L50: to do: trans.isglobal needs another variant trans.apply_from_here
 tm_shape(Africa, crs = "+proj=robin") +
 	tm_cartogram(size = "pop_est", options = opt_tm_cartogram(itermax = 15)) +
 	tm_text("name")
-
 
 tm_shape(Africa, crs = "+proj=robin") +
 	tm_cartogram_ncont(size = "pop_est", options = opt_tm_cartogram_ncont()) +
@@ -15,3 +14,4 @@ tm_shape(Africa, crs = "+proj=robin") +
 tm_shape(World, crs = "+proj=robin") +
 	tm_polygons() +
 	tm_cartogram_ncont(size = "pop_est", fill = "yellow")
+}
